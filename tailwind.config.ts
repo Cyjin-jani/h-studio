38be784cfd907d249d2a8e722f80cc6857dcff58
Yeonjin <111411106+yjdev91@users.noreply.github.com>
2024-04-27 16:48:16 +0900

    u: tailwind 설정 변경

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import plugin from "tailwindcss/plugin";
 
 const config: Config = {
   content: [
@@ -10,11 +11,33 @@ const config: Config = {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addUtilities, e }) {
+      addUtilities({
+        [`.${e("?")}`]: {
+          animation: `${e("?")}wobble 0.5s ease-in-out alternate infinite`,
+        },
+        [`@keyframes ${e("?")}wobble`]: {
+          "0%": {
+            "box-shadow": "inset 4px 4px rgb(236, 15, 170), inset -4px -4px rgb(236, 15, 170)",
+          },
+          "100%": {
+            "box-shadow": "inset 8px 8px rgb(236, 15, 170), inset -8px -8px rgb(236, 15, 170)",
+          },
+        },
+        ".drag-none": {
+          "-webkit-user-drag": "none",
+          "-khtml-user-drag": "none",
+          "-moz-user-drag": "none",
+          "-o-user-drag": "none",
+          "user-drag": "none",
+        },
+      });
+    }),
+  ],
 };
 export default config;
